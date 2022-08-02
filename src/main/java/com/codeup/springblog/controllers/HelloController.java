02b63cb2d00f0d5c754296a6c9dd9df0ad5fa7f8
Shane Banks <shane.banks02@gmail.com>
2022-08-02 15:09:02 -0500

    lecture work

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -1,18 +1,21 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
 	@GetMapping("/hello")
-	@ResponseBody
-	public String hello() {return "<h1>Hello from Spring</h1>";}
+	public String hello(Model model) {
+		model.addAttribute("name", "World");
+		return "hello";
+	}
 
 	@RequestMapping(path = "/hello/{name}", method = RequestMethod.GET)
-	@ResponseBody
-	public String helloToYou(@PathVariable String name) {
-		return String.format("Nice to meet you, %s!", name);
+	public String helloToYou(@PathVariable String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello";
 	}
 
 	String fizzBuzzEvaluation(int num) {
--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -2,11 +2,16 @@ package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 	@GetMapping("/")
-	@ResponseBody
-	public String hello() {return "<h1>This is Spar...I mean, the landing page!</h1>";}
+	public String welcomePage(){
+		return "home";
+	}
+
+
+
+
+
 }
