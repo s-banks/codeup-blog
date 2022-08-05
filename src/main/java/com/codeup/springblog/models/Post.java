4a7178bfb8b74e93b53e69014ed4c3b7ceaeceed
Shane Banks <shane.banks02@gmail.com>
2022-08-05 12:48:21 -0500

    exercise complete

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -16,6 +16,10 @@ public class Post {
 	@Column(nullable = false)
 	private String body;
 
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
 	public Post() {}
 
 	public Post(String title, String body) {
@@ -23,10 +27,24 @@ public class Post {
 		this.body = body;
 	}
 
+	public Post(String title, String body, User user) {
+		this.title = title;
+		this.body = body;
+		this.user = user;
+	}
+
 	public Post(long id, String title, String body) {
 		this.id = id;
 		this.title = title;
 		this.body = body;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 	public long getId() {
