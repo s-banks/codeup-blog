263fc3b7652717fd8d913c2ed9067ac2341a755a
Shane Banks <shane.banks02@gmail.com>
2022-08-05 10:50:02 -0500

    relationships lecture complete (dogs)

---
+++ src/main/java/com/codeup/springblog/models/Dog.java
@@ -0,0 +1,69 @@
+package com.codeup.springblog.models;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "dogs")
+public class Dog {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(length = 50)
+	private String breed;
+
+	@Column(length = 50, nullable = false)
+	private String name;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "dog")
+	private List<Toy> toys;
+
+
+	public Dog() {
+	}
+
+	public Dog(long id, String breed, String name, List<Toy> toys) {
+		this.id = id;
+		this.breed = breed;
+		this.name = name;
+		this.toys = toys;
+	}
+
+	public Dog(String breed, String name, List<Toy> toys) {
+		this.breed = breed;
+		this.name = name;
+		this.toys = toys;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Toy> getToys() {
+		return toys;
+	}
+
+	public void setToys(List<Toy> toys) {
+		this.toys = toys;
+	}
+}
---
+++ src/main/java/com/codeup/springblog/repositories/ToyRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.springblog.repositories;
+
+import com.codeup.springblog.models.Toy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ToyRepository extends JpaRepository<Toy, Long> {
+}
