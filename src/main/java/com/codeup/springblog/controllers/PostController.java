e04f5766d139641266c87984008f8718d5e9cf3d
Shane Banks <shane.banks02@gmail.com>
2022-08-08 15:56:43 -0500

    Services exercise done

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -2,9 +2,11 @@ package com.codeup.springblog.controllers;
 
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositories.PostRepository;
 import com.codeup.springblog.repositories.TagRepository;
 import com.codeup.springblog.repositories.UserRepository;
+import com.codeup.springblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,12 +18,15 @@ public class PostController {
 	private PostRepository postsDao;
 	private UserRepository usersDao;
 	private TagRepository tagDao;
+	private EmailService emailService;
+	//private UserRepository getUsersDao;
 
-	public PostController(PostRepository postsDao, UserRepository usersDao, TagRepository tagDao) {
+	public PostController(PostRepository postsDao, UserRepository usersDao, TagRepository tagDao, EmailService emailService) {
 		this.postsDao = postsDao;
 		this.usersDao = usersDao;
 		this.tagDao = tagDao;
-
+		this.emailService = emailService;
+		//this.getUsersDao = getUsersDao;
 	}
 
 	@RequestMapping(path = "/posts", method = RequestMethod.GET)
@@ -44,13 +49,18 @@ public class PostController {
 	}
 
 	@PostMapping(path = "/posts/create")
-	String createPost(@ModelAttribute Post post) {
+	String createPost(@ModelAttribute Post post, Model model) {
 		postsDao.save(post);
+		 User user2 = post.getUser();
+		 String user3 = user2.getUsername();
+		model.addAttribute("users", usersDao.findAll());
+		emailService.prepareAndSend(usersDao.findByUsername(user3), "Post Created", "Your new post has been created.");
 		return "redirect:/posts";
 	}
 
 	@GetMapping("/posts/edit/{id}")
 	public String editProduct(Model model, @PathVariable long id) {
+		model.addAttribute("users", usersDao.findAll());
 		model.addAttribute("post", postsDao.getById(id));
 		return "posts/create";
 	}
---
+++ src/main/java/com/codeup/springblog/services/EmailService.java
@@ -0,0 +1,51 @@
+package com.codeup.springblog.services;
+
+import com.codeup.springblog.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("mailService")
+public class EmailService {
+	@Autowired
+	public JavaMailSender emailSender;
+
+	@Value("${spring.mail.from}")
+	private String from;
+
+	public JavaMailSender getEmailSender() {
+		return emailSender;
+	}
+
+	public void setEmailSender(JavaMailSender emailSender) {
+		this.emailSender = emailSender;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public void prepareAndSend(User user, String subject, String body) {
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setFrom(from);
+		msg.setTo(user.getEmail());
+		msg.setSubject(subject);
+		msg.setText(body);
+
+		try{
+			this.emailSender.send(msg);
+		}
+		catch (MailException ex) {
+			// simply log it and go on...
+			System.err.println(ex.getMessage());
+		}
+
+	}
+}
